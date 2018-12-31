450b6ecee64e3b2f643c606de02a7b1005f8724f
sd1998 <shashvat51@gmail.com>
2018-12-31 12:01:18 +0530

    Bug fixes

--- functions/index.js
+++ functions/index.js
@@ -2,43 +2,44 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 var async = require('asyncawait/async')
 var await = require('asyncawait/await')
+const config = require('./config.js')
 admin.initializeApp()
 
 exports.sendMessage = functions.https.onRequest(async ((req,res) => {
+  if(req.body.api_key == config.API_KEY){
   const payload = {
-    "data": {
-      heading: req.body.heading,
-      content: req.body.content
+    data: {
+      "heading": req.body.heading+"",
+      "content": req.body.content+""
     }
   }
-  const allTokens = await (admin.database().ref('ref').once('value'))
-  var resp = {
-    "result": "Tokens not present"
-  }
+  const allTokens = await (admin.database().ref('/data/fcm_tokens').once('value'))
   if(allTokens.exists()){
     const data = allTokens.val()
-    const keys = Object.keys(allTokens.val())
+    const keys = Object.keys(data)
     const tokens = []
     for(var i=0;i<keys.length;i++){
       tokens.push(data[keys[i]])
     }
-    console.log(tokens)
-    const response = admin.messaging().sendToDevice(tokens,payload)
+    const response = await (admin.messaging().sendToDevice(tokens,payload))
     await (cleanUp(response, tokens,keys))
-    resp["result"] = "Success"
   }
-    res.status(304).send(resp)
+    res.status(304).send("Success")
+  } 
+  else{
+    res.status(401).send("Unauthorized")
+  }
   }));
 
 function cleanUp(response,tokens,keys){
   const tokensToRemove = {}
   response.results.forEach((result,index) => {
     const error = result.error
-    if(err){
+    if(error){
       console.error('Failure sending notification to', tokens[index], error)
       if (error.code === 'messaging/invalid-registration-token' ||
          error.code === 'messaging/registration-token-not-registered') {
-       tokensToRemove[`/data/fcmTokens/${keys[index]}`] = null;
+       tokensToRemove[`/data/fcm_tokens/${keys[index]}`] = null;
      }
     }
   })
